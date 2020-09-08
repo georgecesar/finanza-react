033eb00b22382ea87a0aefc7ca77498826089efa
georgecesar <giorgio.cesaroni@me.com>
2020-09-07 20:27:43 -0400

    Updates

---
+++ precache-manifest.6795c66a1c5c2a7249cc6c31d45099ed.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e859bf836309c9662ca5210b375cab5f",
+    "url": "/finanza-react/index.html"
+  },
+  {
+    "revision": "f8ab311ea773afe08113",
+    "url": "/finanza-react/static/css/main.867cf627.chunk.css"
+  },
+  {
+    "revision": "fd403dfcf32d045bbc32",
+    "url": "/finanza-react/static/js/2.f551d40a.chunk.js"
+  },
+  {
+    "revision": "70d88436874ad6f0b4ff8dc0e5c19c4e",
+    "url": "/finanza-react/static/js/2.f551d40a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f8ab311ea773afe08113",
+    "url": "/finanza-react/static/js/main.fecb95ec.chunk.js"
+  },
+  {
+    "revision": "7da7a42025a8a49f81db",
+    "url": "/finanza-react/static/js/runtime-main.909e7a13.js"
+  }
+]);
